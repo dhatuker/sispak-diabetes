4a47d19f0228345df42f7120d0d7d0e2df6433f7
dhatuker <dhatukertayuga@gmail.com>
2020-11-24 12:22:09 +0700

    edit UI/UX, now more interactive

--- app/src/main/java/com/example/diabetes_sispak/MainActivity.java
+++ app/src/main/java/com/example/diabetes_sispak/MainActivity.java
@@ -2,13 +2,32 @@ package com.example.diabetes_sispak;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button = (Button)findViewById(R.id.buttonStart);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openQuestion();
+            }
+        });
     }
+
+    public void openQuestion(){
+        Intent intent = new Intent(this, MainQuestion.class);
+        startActivity(intent);
+    }
+
+
 }
