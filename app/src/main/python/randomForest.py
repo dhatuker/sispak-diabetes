ed7f30c0ae79fe8de0b66929ae34469724228efe
dhatuker <dhatukertayuga@gmail.com>
2020-12-01 12:53:49 +0700

    add RF from python

---
+++ app/src/main/python/randomForest.py
@@ -0,0 +1,32 @@
+import pandas as pd
+import numpy as np
+from sklearn.preprocessing import StandardScaler
+from sklearn.ensemble import RandomForestClassifier
+
+def random_forest(input):
+    # coba nanti pathnya diganti '../app/res/raw/diabetes_data_upload.csv', kalo masih gabisa coba ditambah os yang add path
+    dataset = pd.read_csv('E:/Kuliah Semester 7/Sistem Pakar/RF/diabetes_data_upload.csv')
+    data = dataset.replace({'Yes': 1, 'No': 0, 'Male': 1, 'Female': 0, 'Positive': 1, 'Negative': 0})
+
+    x_train = data.iloc[1:501, 0:16].values
+    y_train = data.iloc[1:501, 16].values
+
+    sc = StandardScaler()
+    x_train = sc.fit_transform(x_train)
+    x_test = sc.transform(input)
+
+    for i in range(1, 5):
+        rc = RandomForestClassifier(n_estimators=i, criterion='entropy', random_state=0)
+        rc.fit(x_train, y_train)
+
+    y_pred = rc.predict(x_test)
+    hasil = ""
+    if y_pred[0] == 1:
+        hasil = "Positive"
+    elif y_pred[0] == 0:
+        hasil = "Negative"
+    return hasil
+
+# ini script buat ngejalanin, x_test harus bentuknya 2D jadi kurungnya ada 2
+x_test = [[66,  1,  1,  0,  1,  0,  0,  1,  0,  1,  1,  0,  0,  0,  1,  0]]
+print(random_forest(x_test))
